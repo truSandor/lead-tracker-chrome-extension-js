f68086f35fcecac39c7ef6c51ab138eb5122ebc9
T.Sanyi <s.truczkai@gmail.com>
2023-08-08 14:35:08 +0200

    implement reading data from localStorage

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
+const myLeadsFromStorage = localStorage.getItem("myLeads")
 
-let myLeads = []
+let myLeads = myLeadsFromStorage ? JSON.parse(myLeadsFromStorage) : [];
 
-function renderLeads() {
+inputBtn.addEventListener("click", function () {
+    const inputText = inputEl.value
+    myLeads.push(inputText)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    addListItemToHTML(inputText);
+})
+
+function addListItemToHTML(inputText) {
+    const li = document.createElement("li")
+    const a = document.createElement("a")
+    a.textContent = inputText
+    a.setAttribute('href', `//${inputText}`)
+    a.setAttribute("target", "_blank")
+    li.appendChild(a);
+    ulEl.append(li)
+    inputEl.value = ""
+}
+
+function printListFromLocalStorage() {
     let listItems = ""
     myLeads.forEach(lead => listItems += `
                                         <li>
@@ -17,14 +36,4 @@ function renderLeads() {
     ulEl.innerHTML = listItems
 }
 
-renderLeads()
-
-inputBtn.addEventListener("click", function () {
-    myLeads.push(inputEl.value)
-    const li = document.createElement("li")
-    li.textContent = inputEl.value
-    ulEl.append(li)
-    inputEl.value = ""
-})
-
-
+printListFromLocalStorage()
