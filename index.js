dc2dfcf2f7469bd0aa3082daaed2ad15ef9938de
T.Sanyi <s.truczkai@gmail.com>
2023-08-08 12:56:53 +0200

    refactor the button fuction

--- index.js
+++ index.js
@@ -2,11 +2,29 @@ const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
-let myLeads = ["one", "two", "three"]
+let myLeads = ["www.awesomeleads.com", "www.awesomeleads2.com", "www.awesomeleads3.com"]
+
+function renderLeads() {
+    let listItems = ""
+    myLeads.forEach(lead => listItems += `
+                                        <li>
+                                            <a target='_blank' href='//${lead}'>
+                                                ${lead}
+                                            </a>
+                                        </li>
+                                        `
+    )
+    ulEl.innerHTML = listItems
+}
+
+renderLeads()
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
-    myLeads.forEach(record => ulEl.innerHTML += "<li>" + record + "</li>")
+    const li = document.createElement("li")
+    li.textContent = inputEl.value
+    ulEl.append(li)
+    inputEl.value = ""
 })
 
 
